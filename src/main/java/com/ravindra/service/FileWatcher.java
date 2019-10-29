f5a3101d6cabd588f557efebd2fc0d0030539ad8
Kakarla V S Ravindra Reddy <kvsravindrareddy@gmail.com>
2019-10-29 07:59:03 +0530

    test

--- src/main/java/com/ravindra/service/FileWatcher.java
+++ src/main/java/com/ravindra/service/FileWatcher.java
@@ -9,17 +9,37 @@ import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties
 public class FileWatcher {
+	private String dirPath;
+	
+	private String filePath;
+	
+	public String getDirPath() {
+		return dirPath;
+	}
+	public void setDirPath(String dirPath) {
+		this.dirPath = dirPath;
+	}
+	public String getFilePath() {
+		return filePath;
+	}
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
 	@Bean
 	public void watchFile() {
 		try (WatchService watchService = FileSystems.getDefault().newWatchService()) {
-			Paths.get("D://Learning").register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+			Paths.get(dirPath).register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
 //			while (true) {
-			File path = new File("D://Learning/test.csv");
+			File path = new File(filePath);
 			if (path.exists()) {
 				System.out.println("File is exist in the given directory");
 			}
